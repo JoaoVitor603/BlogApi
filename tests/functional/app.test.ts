92f69212069452a2d95d660cc4d3c08a9f4bd014
João Vitor Lopes <joaovitor.lopes@softdesign.com.br>
2022-10-07 14:59:12 -0300

    fix: quickstart-react corrigido mensagem
    Change-Id: Ie53be16d6233fc652e3aa009036e398419677288

--- tests/functional/app.test.ts
+++ tests/functional/app.test.ts
@@ -2,7 +2,7 @@ import supertest from 'supertest';
 import app from '../../src/app';
 
 describe('App', () => {
-  it('should return 200', async () => {
+  it('it should return 200', async () => {
     const { status } = await supertest(app).get('/api/healthcheck');
     expect(status).toBe(200);
   });
